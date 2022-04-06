6e001e23b76ae8fbe431ea10c0ae9ca2ad59a9ae
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-06 15:23:38 +0000

    Add models structure for posting and commenting

--- aromaticblog/models.py
+++ aromaticblog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    content = models.TextField()
+    related_img = CloudinaryField('image', default='placeholder')
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    excerpt = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'A post named {self.title} was posted by {self.author}'
+
+    def num_of_likes(self):
+        return self.likes.count()
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
