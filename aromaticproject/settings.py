d179a9cbfe1a9b6c5c5e100b3081c1330cb484a7
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-07 00:10:03 +0000

    Changed navbar to start redesigning

--- aromaticproject/settings.py
+++ aromaticproject/settings.py
@@ -17,7 +17,7 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
