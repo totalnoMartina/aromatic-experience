b928672b171245b946a66f67ef6aa398b12f33ec
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-08 18:55:21 +0000

    Fix method on Facebook login, tested half of views

--- aromaticblog/test_views.py
+++ aromaticblog/test_views.py
@@ -1,6 +1,18 @@
 """ A django module for testing """
 from django.test import TestCase
-from .views import ListOfPosts, PostLike, Detail
+from .models import Post
 
 
 class TestViews(TestCase):
+    """ For testing the views functionality """
+    def get_posts_display_in_homepage(self):
+        """ Test passes if display of posts is on a homepage """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def get_posts_details_page(self):
+        """ For testing if there is post rendering """
+        response = self.client.get('/<slug:slug>', )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'detail_view.html')
