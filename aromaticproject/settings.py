6afad34b1b44de3aa5b85171d5d14cd508a3401d
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-09 20:05:04 +0000

    Change condition on DATABASES to fix Syntax error

--- aromaticproject/settings.py
+++ aromaticproject/settings.py
@@ -148,7 +148,7 @@ if 'DATABASE_URL' in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else 'test' in sys.argv:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
