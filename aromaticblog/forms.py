aa1b7b18f8f1c2da1ee01a6f24d818055cc34a28
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-25 14:41:19 +0000

    Add widget template, widgets.py, not working currently yet

--- aromaticblog/forms.py
+++ aromaticblog/forms.py
@@ -1,14 +1,24 @@
 """ A django module to manipulate models """
 from django import forms
 from .models import Comment, Post
+from .widgets import CustomClearableFileInput
 
 
 class PostForm(forms.ModelForm):
+
+    
     class Meta:
         """ A class for displaying body and name of a comment"""
         model = Post
-        fields = ('title', 'content', 'related_img', 'slug', 'excerpt', )
-
+        fields = '__all__'
+        image = forms.ImageField(label='image', required=False,
+                             widget=CustomClearableFileInput)
+    def __init__(self, *args, **kwargs):
+        """ A function that hides the item and user
+        that is updating post so it canot be modified """
+        super().__init__(*args, **kwargs)
+        self.fields['author'].widget = forms.HiddenInput()
+        self.fields['likes'].widget = forms.HiddenInput()
 
 class CommentForm(forms.ModelForm):
     """ a model for the comment form """
