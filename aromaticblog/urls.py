c5707eb4754d55bf5065dbb9ac1d35742a405299
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-26 16:17:39 +0000

    Add username to urls, templates, views within functions, checking the author of post to be user

--- aromaticblog/urls.py
+++ aromaticblog/urls.py
@@ -4,14 +4,11 @@ from .views import ListOfPosts, Detail, TheLikes, PostUpdate, UsersDraftPost, Dr
 urlpatterns = [
     path('', ListOfPosts.as_view(), name='home'),
     path('drafts/<str:username>', UsersDraftPost.as_view(), name='drafts'),
-    path('drafts_detail/<slug:slug>/', DraftDetail.as_view(), name='draft_detail'),
+    path('drafts_detail/<str:username>/<slug:slug>/', DraftDetail.as_view(), name='draft_detail'),
     path('<slug:slug>/', Detail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', TheLikes.as_view(), name='post_like'),
     path('add_post/<slug:slug>/', PostAdding.as_view(), name='add_post'),
-    path('edit_post/<slug:slug>/', PostUpdate.as_view(), name='edit_post'),
-    path('post_confirm_delete/<slug:slug>/', PostDelete.as_view(), name='post_confirm_delete'),
+    path('edit_post/<str:username>/<slug:slug>/', PostUpdate.as_view(), name='edit_post'),
+    path('post_confirm_delete/<str:username>/<slug:slug>/', PostDelete.as_view(), name='post_confirm_delete'),
 
     # path('update/<slug:slug>/comment_id', views.update_comment, name='update_comment'),
-
-
-]
