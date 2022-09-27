04d306184abad935bd32e7543ce9a142297e71ba
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-27 06:03:39 +0000

    Add import of slugify

--- aromaticblog/models.py
+++ aromaticblog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+#import slugify from django utils
+from django.utils.text import slugify
 
 # Status of the posts, so that if not ready to be published, is stored
 STATUS = ((0, 'Draft'), (1, 'Published'))
