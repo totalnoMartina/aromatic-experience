1a69a5d2ad8a4d04d052c083a6e895d78c613ef2
totalnoMartina <totalnobzvz@yahoo.com>
2022-11-17 21:23:52 +0000

    Add build.sh file and content

--- aromaticproject/settings.py
+++ aromaticproject/settings.py
@@ -19,6 +19,11 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'  # The page can be displayed in a frame of same o
 
 ALLOWED_HOSTS = ['aromatic-martina.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
